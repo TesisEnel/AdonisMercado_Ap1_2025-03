b27d9ab1aa93b28aa1fd6d2bb5e0e824a37e4777
adonismercado <adonis_mercado@ucne.edu.do>
2025-12-01 10:55:16 -0400

    feat ✨: añadir clase ToastServiceExtensions con método ShowToast para manejo unificado de toasts

---
+++ ProyectoFinal_AP1_AdonisMercado/Extensors/ToastServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BlazorBootstrap;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+namespace ProyectoFinal_AP1_AdonisMercado.Extensors;
+
+public class ToastServiceExtensions
+{
+    public static ToastMessage ShowToast(this ToastService toastService, ToastType toastType, string title, string customMessage = null)
+    {
+        var message = new ToastMessage
+        {
+            Type = toastType,
+            Title = title,
+            Message = customMessage ?? $"A las {DateTime.Now.ToString("hh:mm tt")}"
+        };
+        toastService.Notify(message);
+        return message;
+    }
+}
