172623325d31fb4e294d66b8ca80373ca2284db3
adonismercado <adonis_mercado@ucne.edu.do>
2025-12-14 01:49:28 -0400

    feat ✨: agregar constructor con IAmazonS3 para inyección de dependencias y pruebas unitarias a CloudflareR2Services
    Permite inyectar un cliente S3 externo en CloudflareR2Service,
    facilitando las pruebas unitarias con mocks sin afectar el
    comportamiento en producción.

--- ProyectoFinal_AP1_AdonisMercado/Services/CloudflareR2Services.cs
+++ ProyectoFinal_AP1_AdonisMercado/Services/CloudflareR2Services.cs
@@ -7,9 +7,16 @@ namespace ProyectoFinal_AP1_AdonisMercado.Services;
 
 public class CloudflareR2Service
 {
-    private readonly AmazonS3Client _s3Client;
+    private readonly IAmazonS3 _s3Client;
     private readonly string _bucketName;
     private readonly string _publicUrl;
+
+    public CloudflareR2Service(IAmazonS3 s3client, IConfiguration configuration)
+    {
+        _s3Client = s3client;
+        _bucketName = configuration["CloudflareR2:BucketName"]!;
+        _publicUrl = configuration["CloudflareR2:PublicUrl"]!;
+    }
 
     public CloudflareR2Service(IConfiguration configuration)
     {
